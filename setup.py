cc7e78e7cc89ed1829922cfd25c57bcc3504b7e0
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-07-13 10:53:04 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: LInux",
+    ],
+    name="pipelineModules",
+    version="0.0.1",
+    package_dir={"": "src"},
+    requires=[
+	'numpy',
+	'Pillow',
+	'dask',
+	'git+https://github.com/ScalableImagingPipeline/dask-jobqueue.git#egg=dask_jobqueue'
+    ]
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
