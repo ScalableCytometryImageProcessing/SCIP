72cc4752439afb63ba0c0dde0977ea309cff6d4d
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-07-13 13:13:58 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setuptools.setup(
     name="pipelineModules",
     version="0.0.1",
     package_dir={"": "src"},
-    requires=[
+    install_requires=[
 	'numpy',
 	'Pillow',
 	'dask',
-	'git+https://github.com/ScalableImagingPipeline/dask-jobqueue.git#egg=dask_jobqueue'
-    ]
+	'dask_jobqueue'
+    ],
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
 )
