bd6af328ed3141fa57102d23b3c97c4aed3a87e8
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-07-30 11:20:00 +0200

    Store pandas to parquet

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 source $CONDA_PREFIX/etc/profile.d/conda.sh
 
-mamba create -n scip python=3.8 &&\
+mamba create -y -n scip python=3.8 &&\
 conda activate scip &&\
-mamba install dask &&\
+mamba install -y dask &&\
 pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04/ cellprofiler &&\
 pip install -e . &&\
 pip install -r requirements.txt
