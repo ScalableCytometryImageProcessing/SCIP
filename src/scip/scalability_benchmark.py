80e3dcdfc3ccafc38780d37fb7851ec654aedb83
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-08-26 11:33:05 +0200

    sequential benchmark

--- src/scip/scalability_benchmark.py
+++ src/scip/scalability_benchmark.py
@@ -4,10 +4,11 @@ from datetime import datetime
 import uuid
 import json
 import logging
-import asyncio
+import subprocess
+import shlex
 
 
-async def main():
+def main():
 
     output = Path.cwd() / Path("benchmark_%s" % datetime.now().strftime("%Y%m%d%H%M%S"))
     output.mkdir()
@@ -17,45 +18,36 @@ async def main():
     logger = logging.getLogger(__name__)
 
     paths = " ".join([
-        "/home/maximl/shared_scratch/vulcan_pbmc"
+        "/home/maximl/shared_scratch/vulcan_pbmc_debug"
     ])
-    iterations = 5
+    iterations = 3
     n_workers = 1
 
     timings = []
-    subprocs = []
-    for n_processes in [1, 2, 4, 8, 16, 32]:
+    for n_processes in [1, 2, 4, 8, 16]:
         logger.info(f"Benchmarking {n_processes}")
         for i in range(iterations):
             logger.info(f"{n_processes}: iteration {i+1}/{iterations}")
             
             ident = uuid.uuid4()
             timing = str(output / ("%s.json" % ident))
-            timings.append(timing)
 
             o = str(output / "results" / str(ident))
             command = f"scip -j{n_workers} -n{n_processes} --no-local "
             command += f"--headless --timing {timing} -o {o} scip.yml {paths}"
 
             logger.info(f"Launching: {command}")
-            proc = await asyncio.create_subprocess_shell(command)
-            subprocs.append(proc.wait())
-
-            if len(subprocs) >= 2:
-                logger.info(f"Waiting for next {len(subprocs)} tasks to finish")
-                await asyncio.gather(*subprocs)
-                subprocs = []
-
-    logger.info(f"Waiting for final {len(subprocs)} tasks to finish")
-    await asyncio.gather(*subprocs)
+            ret = subprocess.run(shlex.split(command))
+            timings.append((ret.returncode, timing))
 
     timing_data = []
-    for timing in timings:
-        with open(timing) as fp:
-            timing_data.append(json.load(fp))
+    for ret, timing in timings:
+        if ret == 0:
+            with open(timing) as fp:
+                timing_data.append(json.load(fp))
     pandas.DataFrame.from_records(timing_data).to_csv(
         str(output / 'timing_results.csv'), index=False)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
