0bd21bb93eb5661fca943748eca46723aae5f81a
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-08-27 14:49:12 +0200

    Benchmark update

--- src/misc/benchmark_setup.py
+++ src/misc/benchmark_setup.py
@@ -11,23 +11,24 @@ def main():
     (output / "results").mkdir()
     
     paths = " ".join([
-        "/home/maximl/shared_scratch/vulcan_pbmc_1000"
+        "/home/maximl/shared_scratch/vulcan_pbmc"
     ])
     iterations = 3
-    n_workers = 1
 
     commands = []
-    for n_processes in [2, 4, 8, 16, 32]:
-        for _ in range(iterations): 
-            ident = uuid.uuid4()
-            timing = str(output / ("%s.json" % ident))
+    for partition_size in [10, 50, 100]:
+        for n_workers in [2, 3, 4]:
+            for n_processes in [2, 4, 8, 16, 24]:
+                for _ in range(iterations): 
+                    ident = uuid.uuid4()
+                    timing = str(output / ("%s.json" % ident))
 
-            o = str(output / "results" / str(ident))
-            command = f"scip -j{n_workers} -n{n_processes} --no-local "
-            command += f"--headless --timing {timing} --partition-size 50 "
-            command += f"{o} scip.yml {paths}"
+                    o = str(output / "results" / str(ident))
+                    command = f"scip -j{n_workers} -n{n_processes} --no-local "
+                    command += f"--headless --timing {timing} --partition-size {partition_size} "
+                    command += f"{o} scip.yml {paths}"
 
-            commands.append(command)
+                    commands.append(command)
 
     with open(str(output / "run.sh"), "w") as fh:
         fh.write("#!/bin/bash\n")
