c39152ad093a2a851fed8c72f463fe58d6ae2602
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-08-27 11:08:27 +0200

    Remove debugging

--- src/misc/benchmark_setup.py
+++ src/misc/benchmark_setup.py
@@ -10,21 +10,15 @@ def main():
     output.mkdir()
     (output / "results").mkdir()
     
-    logging.basicConfig(level=logging.INFO, filename=str(output / "benchmark.log"))
-    logger = logging.getLogger(__name__)
-
     paths = " ".join([
-        "/home/maximl/shared_scratch/vulcan_pbmc_debug"
+        "/home/maximl/shared_scratch/vulcan_pbmc_1000"
     ])
     iterations = 3
     n_workers = 1
 
     commands = []
-    for n_processes in [1, 2, 4, 8, 16, 32]:
-        logger.info(f"Benchmarking {n_processes}")
-        for i in range(iterations):
-            logger.info(f"{n_processes}: iteration {i+1}/{iterations}")
-            
+    for n_processes in [2, 4, 8, 16, 32]:
+        for _ in range(iterations): 
             ident = uuid.uuid4()
             timing = str(output / ("%s.json" % ident))
 
