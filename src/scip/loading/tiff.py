dbbc8989772b97787b0e699021a1458d2534aaef
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-09-21 14:51:10 +0200

    Put filter after pivot

--- src/scip/loading/tiff.py
+++ src/scip/loading/tiff.py
@@ -7,10 +7,15 @@ import logging
 logging.getLogger("tifffile").setLevel(logging.ERROR)
 
 
-def load_image(event, channels): 
-    paths = [event[str(c)] for c in channels]
-    arr = tifffile.imread(paths) / 2**12
-    return dict(pixels=arr, path=paths, idx=event["idx"])
+def load_image(event, channels):
+    try: 
+        paths = [event[str(c)] for c in channels]
+        arr = tifffile.imread(paths) / 2**12
+        return dict(pixels=arr, path=paths, idx=event["idx"])
+    except TypeError as e:
+        logging.getLogger(__name__).exception(e)
+        logging.getLogger(__name__).error(paths)
+        raise e
 
 
 def bag_from_directory(*, path, idx, channels, partition_size, regex):
@@ -31,13 +36,14 @@ def bag_from_directory(*, path, idx, channels, partition_size, regex):
     
     matches = list(filter(lambda r: r is not None, map(match, path.glob("**/*.tif*"))))
     df = pandas.DataFrame.from_dict(matches)
+    
+    df = df.pivot(index="id", columns="channel", values="path")
 
     pre_filter = len(df)
     df = df.dropna(axis=0, how="any")
     dropped = pre_filter - len(df)
-    logger.warning("Dropped %d rows, because of missing channel files." % dropped)
+    logger.warning("Dropped %d rows because of missing channel files in %s." % (dropped, str(path)))
 
-    df = df.pivot(index="id", columns="channel", values="path")
     df["idx"] = pandas.RangeIndex(start=idx, stop=idx+len(df))
 
     bag = dask.bag.from_sequence(df.to_dict(orient="records"), partition_size=partition_size)
