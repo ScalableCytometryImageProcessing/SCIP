3aad86c0ea528196f038a9c64a131e6707bdc0ad
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-09-29 15:26:00 +0200

    Implement new benchmark setup

--- src/misc/benchmark_setup.py
+++ src/misc/benchmark_setup.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import datetime
 import uuid
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def main():
@@ -9,33 +10,39 @@ def main():
     output.mkdir()
     (output / "results").mkdir()
 
-    paths = " ".join([
-        "/data/gent/vo/000/gvo00070/vsc42015/datasets/vib/vulban_pbmc"
-    ])
     iterations = 3
+    total_mem = 128
+
+    args_fmt = "--timing %s -j%d -m%d -s%d %s"
 
     commands = []
+    np = []
     for partition_size in [10, 50, 100]:
-        for n_threads, n_workers in zip(
-            [2**i for i in range(5)],
-            reversed([2**i for i in range(5)])
-        ):
+        for n_workers in [1, 2, 4, 8, 16, 28]:
             for _ in range(iterations):
                 ident = uuid.uuid4()
+
                 timing = str(output / ("%s.json" % ident))
-
                 o = str(output / "results" / str(ident))
-                command = f'scip -j{n_workers} -t{n_threads} --no-local '
-                command += '-c28 -m64 -w00:45:00 -e "-A lt1_starter-245" '
-                command += f'--headless --timing {timing} --partition-size {partition_size} '
-                command += f'-d /local_scratch/ {o} scip.yml {paths}'
+                
+                commands.append(
+                    args_fmt % (timing, n_workers, total_mem // n_workers, partition_size, o)
+                )
+                np.append(n_workers)
 
-                commands.append(command)
 
-    with open(str(output / "run.sh"), "w") as fh:
-        fh.write("#!/bin/bash\n")
-        for command in commands:
-            fh.write(command + "\n")
+
+    jinja_env = Environment(
+        loader=FileSystemLoader("src/misc"),
+        autoescape=select_autoescape()
+    )
+    jinja_template = jinja_env.get_template("qsub_template.pbs")
+    with open(str(output / "qsub.pbs"), "w") as fh:
+        fh.write(jinja_template.render(
+            number_of_configs=len(commands),
+            np_args=",".join([str(c) for c in np]), 
+            args=",".join(commands)
+        ))
 
 
 if __name__ == "__main__":
