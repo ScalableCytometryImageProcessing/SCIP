d95488a5c7ded4f4f19d4a1ea25217db330a7600
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-10-04 10:56:35 +0200

    Update qsub for atools

--- src/misc/benchmark_setup.py
+++ src/misc/benchmark_setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from datetime import datetime
 import uuid
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas 
 
 
 def main():
@@ -14,37 +14,24 @@ def main():
     iterations = 5
     total_mem = 120
 
-    args_fmt = '"--timing %s -j%d -m%d -s%d %s"'
-
     commands = []
-    np = []
     for partition_size in [100, 200, 400, 800]:
         for n_workers in [1, 2, 4, 8, 16, 26]:
             for _ in range(iterations):
                 ident = uuid.uuid4()
 
-                timing = str(output / ("%s.json" % ident))
                 o = str(output / "results" / str(ident))
 
-                commands.append(
-                    args_fmt % (timing, n_workers, total_mem // n_workers, partition_size, o)
-                )
-                
-                # increment n_workers with 2 to accomodate for the client and scheduler process 
-                np.append(n_workers+2)
+                commands.append(dict(
+                    n_workers=n_workers,
+                    memory=total_mem // n_workers,
+                    partition_size=partition_size,
+                    output=o,
+                    np=n_workers+2,
+                    prefix=str(output)
+                ))
 
-    jinja_env = Environment(
-        loader=FileSystemLoader("src/misc"),
-        autoescape=select_autoescape()
-    )
-    jinja_template = jinja_env.get_template("qsub_template.pbs")
-    with open(str(output / "qsub.pbs"), "w") as fh:
-        fh.write(jinja_template.render(
-            number_of_configs=len(commands),
-            np_args=" ".join([str(c) for c in np]), 
-            args=" ".join(commands)
-        ))
-
+    pandas.DataFrame(commands).to_csv(str(output / "data.csv"), index=False)
 
 if __name__ == "__main__":
     main()
