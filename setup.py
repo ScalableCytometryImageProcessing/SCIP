3c236f7d69eb670880f8c70c8ba0bdea0a2e4747
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-10-12 14:11:47 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
         "Operating System :: LInux",
     ],
     name="scip",
-    version="0.0.2",
+    version="0.1.0",
     package_dir={"": "src"},
     install_requires=[
         'numpy',
