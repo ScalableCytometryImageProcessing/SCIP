4922031cb89632a922d457542e8a568f189f6346
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-10-22 11:23:22 +0200

    Bump version for CD7 support

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
         "Operating System :: LInux",
     ],
     name="scip",
-    version="0.1.0",
+    version="0.1.1",
     package_dir={"": "src"},
     install_requires=[
         'numpy',
