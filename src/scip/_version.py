6ff3215c92a0151b9b9a69709b62fed64cb79263
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-01-03 13:04:08 +0100

    Bugfix prefix

--- src/scip/_version.py
+++ src/scip/_version.py
@@ -42,7 +42,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "'v'"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "src/scip/_version.py"
     cfg.verbose = False
