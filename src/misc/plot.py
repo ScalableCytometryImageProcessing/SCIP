e06cb853d3b9671b5a931e7faba886968b0a78f0
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-01-12 10:36:28 +0100

    Plot script for debugging

---
+++ src/misc/plot.py
@@ -0,0 +1,13 @@
+import pandas
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pyarrow.parquet as pq
+
+if __name__ == "__main__":
+    fs = Path('tmp/').glob("*.parquet")
+    df = pandas.concat([pq.read_table(f).to_pandas() for f in fs])
+
+    x = df["feat_area_combined"]
+    y = df["feat_eccentricity_combined"]
+    plt.scatter(x, y, s=1, alpha=0.5)
+    plt.savefig("test.png")
