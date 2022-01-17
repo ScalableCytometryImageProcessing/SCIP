7ced0149a646c2a8260f6a5e2e118c8358a6fcb3
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-01-17 14:42:22 +0100

    Add n_workers test

--- src/misc/benchmark_setup.py
+++ src/misc/benchmark_setup.py
@@ -22,7 +22,7 @@ import pandas
 import os
 
 
-def main():
+def main(a):
 
     output = Path(os.environ["VSC_DATA_VO_USER"]) / "results/scip_benchmark"
     output = output / Path("benchmark_%s" % datetime.now().strftime("%Y%m%d%H%M%S"))
@@ -30,29 +30,58 @@ def main():
     (output / "results").mkdir()
 
     iterations = 3
-    total_mem = 88
-    n_workers = 10
-    partition_size = 200
-
     commands = []
-    for limit in [1000, 10000, 100000]:
-        for _ in range(iterations):
-            ident = uuid.uuid4()
 
-            o = str(output / "results" / str(ident))
+    if a == "size":
+        total_mem = 88
+        n_workers = 10
+        partition_size = 200
 
-            commands.append(dict(
-                n_workers=n_workers,
-                memory=total_mem // n_workers,
-                partition_size=partition_size,
-                output=o,
-                np=n_workers + 2,
-                limit=limit,
-                prefix=str(output)
-            ))
+        for limit in [50000, 200000]:
+            for _ in range(iterations):
+                ident = uuid.uuid4()
+
+                o = str(output / "results" / str(ident))
+
+                commands.append(dict(
+                    n_workers=n_workers,
+                    memory=total_mem // n_workers,
+                    partition_size=partition_size,
+                    output=o,
+                    np=n_workers + 2,
+                    limit=limit,
+                    prefix=str(output)
+                ))
+
+    if a == "n_workers":
+        total_mem = 88
+        partition_size = 200
+        limit = 100000
+
+        for n_workers in [1, 2, 4, 8, 16]:
+            for _ in range(iterations):
+                ident = uuid.uuid4()
+
+                o = str(output / "results" / str(ident))
+
+                commands.append(dict(
+                    n_workers=n_workers,
+                    memory=total_mem // n_workers,
+                    partition_size=partition_size,
+                    output=o,
+                    np=n_workers + 2,
+                    limit=limit,
+                    prefix=str(output)
+                ))
 
     pandas.DataFrame(commands).to_csv(str(output / "data.csv"), index=False)
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", type=str)
+
+    args = parser.parse_args()
+    main(args.a)
