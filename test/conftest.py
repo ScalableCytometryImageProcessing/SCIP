89e372d5e277252c5502d24b42f4c80ce903290c
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-04-05 10:36:06 +0100

    Add zarr testing

--- test/conftest.py
+++ test/conftest.py
@@ -53,7 +53,7 @@ def to_records(images, masks):
 
 # FIXTURES
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def fake_image_nchannels():
     return 3
 
@@ -64,28 +64,41 @@ def fake_images(fake_image_nchannels, n=10):
         numpy.arange(0, 100).reshape(10, 10)[numpy.newaxis], (n, fake_image_nchannels, 1, 1))
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def fake_images_bag(request, fake_images, fake_image_nchannels):
     records = to_records(fake_images, fake_mask(fake_image_nchannels, full=request.param))
     bag = dask.bag.from_sequence(records, partition_size=5)
     return bag
 
 
-@pytest.fixture(scope="session")
-def images_folder():
-    return Path("test/data/images")
+@pytest.fixture
+def fake_images_zarr(fake_images):
+    import zarr
+    z = zarr.array(fake_images.reshape(fake_images.shape[0], -1))
+    z.attrs["shape"] = [i.shape for i in fake_images]
+    return z
 
 
-@pytest.fixture(scope="session")
-def image_path():
-    return Path("test/data/images/pbmc+PI_00000000.tiff")
+@pytest.fixture
+def images_folder(data):
+    return data / "images"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
+def image_path(data):
+    return data / "images/pbmc+PI_00000000.tiff"
+
+
+@pytest.fixture
+def zarr_path(data):
+    return data / "test.zarr"
+
+
+@pytest.fixture
 def data():
     return Path("test/data/")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def config(data):
     return util.load_yaml_config(str(data / "scip.yml"))
---
+++ test/loading/test_zarr.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2022 Maxim Lippeveld
+#
+# This file is part of SCIP.
+#
+# SCIP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SCIP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SCIP.  If not, see <http://www.gnu.org/licenses/>.
+
+from scip.loading import zarr
+import pytest
+
+
+@pytest.mark.parametrize("channels, expected_length", [(None, 7), ([0,1], 2)])
+def test_bag_from_directory(zarr_path, channels, expected_length):
+    bag, meta, length = zarr.bag_from_directory(
+        path=zarr_path, channels=channels, partition_size=5,
+        gpu_accelerated=False, regex="(?P<name>.*)")
+    images = bag.compute()
+
+    assert length == 10
+    assert len(images) == length
+    assert all(len(im["pixels"]) == expected_length for im in images)
+    assert len(meta.keys()) == 4
