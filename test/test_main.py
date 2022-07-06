2103d1a19997a5bb93a86b07403daf26e987a3ae
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-07-06 17:50:34 +0200

    Update test with sampling to limit

--- test/test_main.py
+++ test/test_main.py
@@ -5,14 +5,15 @@ import anndata
 
 
 @pytest.mark.parametrize(
-    'mode,limit',
+    'mode,limit,expected_n',
     [
-        pytest.param('local',1, marks=pytest.mark.mpi_skip),
-        pytest.param('local',-1, marks=pytest.mark.mpi_skip),
-        pytest.param('mpi',1, marks=[
+        pytest.param('local', 2, 2, marks=pytest.mark.mpi_skip),
+        pytest.param('local', 8, 8, marks=pytest.mark.mpi_skip),
+        pytest.param('local', -1, 10, marks=pytest.mark.mpi_skip),
+        pytest.param('mpi', -1, 10, marks=[
             pytest.mark.mpi, pytest.mark.skip(reason="Issues with MPI")])
     ])
-def test_main(mode, limit, zarr_path, tmp_path, data):
+def test_main(mode, limit, expected_n, zarr_path, tmp_path, data):
     runtime = main.main(
         mode=mode,
         n_workers=4,
@@ -21,10 +22,11 @@ def test_main(mode, limit, zarr_path, tmp_path, data):
         output=Path(tmp_path),
         paths=[str(zarr_path)],
         config=data / "scip_zarr.yml",
-        limit=limit
+        limit=limit,
+        partition_size=5
     )
 
     assert runtime is not None
     assert len([f for f in tmp_path.glob("*.h5ad")]) == 10
     assert (tmp_path / "scip.log").exists()
-    assert sum(len(anndata.read(f)) for f in tmp_path.glob("*.h5ad")) > 0
+    assert sum(len(anndata.read(f)) for f in tmp_path.glob("*.h5ad")) == expected_n
