a774b0fb3ee91c13b18cb4dcf54f82b8b0bc8a3c
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-08-30 16:53:10 +0200

    Add option for precomputed correction images

--- src/scip/illumination_correction/jones_2006.py
+++ src/scip/illumination_correction/jones_2006.py
@@ -35,6 +35,26 @@ def _combine(total1, total2):
     )
 
 
+def _finish(total, filter_func, downscale):
+    avg = total[1]["pixels"] / total[1]["count"]
+
+    if downscale > 1:
+        # downscale the image prior to median filtering to reduce memory consumption
+        avg = downscale_local_mean(avg, factors=(1, downscale, downscale))
+
+    avg = numpy.asarray([
+        filter_func(avg[i])
+        for i in range(len(total[1]["pixels"]))
+    ])
+    avg = numpy.where(avg == 0, 1, avg)  # swap out 0 for division no-op 1
+
+    if downscale > 1:
+        # reverse earlier downscaling
+        avg = rescale(avg, scale=downscale, anti_aliasing=True, channel_axis=0)
+
+    return (total[0], avg)
+
+
 def correct(
     *,
     images: dask.bag.Bag,
@@ -43,6 +63,7 @@ def correct(
     median_filter_size: int = 50,
     downscale: int = 1,
     output: Path = None,
+    precomputed: Path = None
 ) -> dask.bag.Bag:
     """
     Distributed implementation of Jones et al. (2006) illumination correction. All images
@@ -57,6 +78,7 @@ def correct(
         median_filter_size: Size of the window used in the median filter.
         downscale: factor by which to downscale the image prior to median filtering
         output: Path pointing to directory to save correction images.
+        precomputed: Path to pickle file with precomputed correction images in a dict.
 
     Returns:
         Collection with corrected images.
@@ -68,41 +90,29 @@ def correct(
     else:
         filter_func = partial(median_filter, size=median_filter_size)
 
-    def finish(total):
-        avg = total[1]["pixels"] / total[1]["count"]
-
-        if downscale > 1:
-            # downscale the image prior to median filtering to reduce memory consumption
-            avg = downscale_local_mean(avg, factors=(1, downscale, downscale))
-
-        avg = numpy.asarray([
-            filter_func(avg[i])
-            for i in range(len(total[1]["pixels"]))
-        ])
-        avg = numpy.where(avg == 0, 1, avg)  # swap out 0 for division no-op 1
-
-        if downscale > 1:
-            # reverse earlier downscaling
-            avg = rescale(avg, scale=downscale, anti_aliasing=True, channel_axis=0)
-
-        return (total[0], avg)
-
     def divide(part, mu):
         newpart = copy.deepcopy(part)
         for x in newpart:
             x["pixels"] = x["pixels"] / mu[x[key]]
         return newpart
 
-    mean_images = images.foldby(
-        key=key,
-        binop=_binop,
-        combine=_combine,
-        initial=dict(pixels=None, count=0),
-        combine_initial=dict(pixels=None, count=0)
-    )
-    mean_images = mean_images.repartition(npartitions=ngroups)
-    mean_images = mean_images.map(finish)
-    mean_images = dask.delayed(dict, pure=True)(mean_images)
+    if precomputed is not None:
+        @dask.delayed(pure=True)
+        def load_images(p):
+            with open(p, "rb") as fh:
+                return pickle.load(fh)
+        mean_images = load_images(str(precomputed))
+    else:
+        mean_images = images.foldby(
+            key=key,
+            binop=_binop,
+            combine=_combine,
+            initial=dict(pixels=None, count=0),
+            combine_initial=dict(pixels=None, count=0)
+        )
+        mean_images = mean_images.repartition(npartitions=ngroups)
+        mean_images = mean_images.map(_finish, filter_func=filter_func, downscale=downscale)
+        mean_images = dask.delayed(dict, pure=True)(mean_images)
 
     images = images.map_partitions(divide, mu=mean_images)
 
