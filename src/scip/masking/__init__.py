20b3e2f8d3bb6dfe98dcb2f47f48379c77aba5a9
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-09-07 14:24:25 +0200

    add all

--- src/scip/masking/__init__.py
+++ src/scip/masking/__init__.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with SCIP.  If not, see <http://www.gnu.org/licenses/>.
 
+__all__ = ["mask"]
+
 from typing import Mapping, Any, List
 
 import numpy
