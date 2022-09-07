a2097e40612ddf8480a21b3c08f75104747f9900
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-09-07 14:29:17 +0200

    Remove all statement

--- src/scip/masking/__init__.py
+++ src/scip/masking/__init__.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with SCIP.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ["mask"]
-
 from typing import Mapping, Any, List
 
 import numpy
