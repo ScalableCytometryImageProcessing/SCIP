c12d2a6fb8bcd7e3d51a6daf83fe348f9cfbc775
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-09-13 23:24:32 +0200

    Move package data and fix rtd

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ if __name__ == "__main__":
     setuptools.setup(
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
+        package_data={
+            'scip': ['utils/logging.yml']
+        }
     )
